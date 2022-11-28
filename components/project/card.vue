95d001813150bcaef584598a6cbc4ea7b82f66d9
ccorcimaru <calincor@gmail.com>
2022-11-28 22:00:27 +0000

    added card component

---
+++ components/project/card.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps<{
+	title: string
+	content: string
+}>()
+</script>
+
+<template>
+	<div class="card-small">
+		<h3>{{ title }}</h3>
+		<div class="flex-col-center">
+			<p>{{ content }}</p>
+		</div>
+	</div>
+</template>
+
+<style scoped lang="scss">
+:root {
+	--colour-highlight: #fff;
+	--card-small-radius: 14px;
+	--card-shadow: 0 4px 4px rgb(0, 0, 0, 0.25);
+}
+
+.card-small {
+	width: 100%;
+	max-width: 30ch;
+	margin: 0.5rem;
+	background-color: var(--colour-highlight);
+	padding: 1rem;
+	border-radius: var(--card-small-radius);
+	box-shadow: var(--card-shadow);
+}
+
+.flex-col-center {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	text-align: center;
+}
+</style>
