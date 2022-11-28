4f09ab1c8034b05ec558597dc4016b289ef53057
ccorcimaru <calincor@gmail.com>
2022-11-28 22:00:08 +0000

    Added initial project page

--- pages/project.vue
+++ pages/project.vue
@@ -1,5 +1,60 @@
-<script setup></script>
+<script setup>
+definePageMeta({
+	name: "Project",
+})
 
-<template></template>
+const projectToggle = ref(false)
 
-<style scoped lang="scss"></style>
+const projects = [
+	{
+		title: "Project 1",
+		content: "This is the first project",
+	},
+	{
+		title: "Project 2",
+		content: "This is the second project",
+	},
+	{
+		title: "Project 3",
+		content: "This is the third project",
+	},
+	{
+		title: "Project 4",
+		content: "This is the fourth project",
+	},
+	{
+		title: "Project 5",
+		content: "This is the fifth project",
+	},
+]
+</script>
+
+<template>
+	<p>The project page</p>
+
+	<button @click="projectToggle = !projectToggle">Show/hide projects</button>
+
+	<p>Currently hide is {{ projectToggle }}</p>
+
+	<div class="project-container">
+		<ProjectCard
+			v-for="project in projects"
+			:title="project.title"
+			:content="project.content"
+			:class="{ hide: projectToggle }"
+		/>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.project-container {
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: center;
+}
+
+.hide {
+	display: none;
+}
+</style>
