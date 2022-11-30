ae56c53143c7e2a5f83c87d895fec2c54cf18b4f
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2022-11-30 14:55:33 +0000

    generic buttons that have an icon next to them

---
+++ components/button.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue"
+defineProps<{
+	icon: string
+}>()
+</script>
+
+<template>
+	<button><slot /> <Icon :icon="icon" /></button>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ components/button/nuxt.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue"
+import { RouteLocationRaw } from "vue-router"
+
+defineProps<{
+	to: RouteLocationRaw
+	icon: string
+}>()
+</script>
+
+<template>
+	<NuxtLink :to="to"
+		><Button :icon="icon"><slot /></Button
+	></NuxtLink>
+</template>
+
+<style scoped lang="scss"></style>
