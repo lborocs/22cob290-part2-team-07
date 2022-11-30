b0b6f1d5d73bf11ccd7da00094738244aaecf9dc
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2022-11-30 17:35:56 +0000

    styled buttons

--- components/button.vue
+++ components/button.vue
@@ -1,12 +1,59 @@
 <script setup lang="ts">
 import { Icon } from "@iconify/vue"
-defineProps<{
+const { icon, shrink = true } = defineProps<{
 	icon: string
+	shrink?: boolean
 }>()
 </script>
 
 <template>
-	<button><slot /> <Icon :icon="icon" /></button>
+	<button :class="{ fix: shrink }">
+		<span id="slot"><slot /></span> <Icon :icon="icon" />
+	</button>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@use "~/assets/core";
+@use "~/assets/mobile";
+
+button {
+	@extend %flex-row, %flex-centre;
+	cursor: pointer;
+	background-color: var(--colour-accent);
+	border: none;
+	margin: 0.1rem 0.5rem;
+	padding: 0.2rem 0.8rem;
+	// width: max-content;
+	transition: {
+		property: color, background-color;
+		duration: 0.1s;
+		timing-function: ease-in-out;
+	}
+
+	border-radius: 0.5rem;
+	font-size: 1.5em;
+	#slot {
+		margin: 0 0.2rem;
+		font-weight: bold;
+	}
+
+	box-shadow: var(--card-shadow);
+	color: var(--colour-background-1);
+
+	&:hover,
+	&:focus {
+		outline-offset: 0.3rem;
+		background-color: var(--colour-highlight);
+		color: var(--colour-accent);
+	}
+
+	@media (max-width: mobile.$width) {
+		aspect-ratio: 1;
+		border-radius: 50%;
+		font-size: 2em;
+		#slot {
+			display: none;
+		}
+	}
+}
+</style>
