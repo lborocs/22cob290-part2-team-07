33c94e010997c1e75f0d87f79669f027aa80f41e
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2022-12-01 01:17:11 +0000

    nuxt button scales correctly with font size changes

--- components/button/nuxt.vue
+++ components/button/nuxt.vue
@@ -2,23 +2,33 @@
 import { Icon } from "@iconify/vue"
 import { RouteLocationRaw } from "vue-router"
 
-defineProps<{
+const { fix = false } = defineProps<{
 	to: RouteLocationRaw
 	icon: string
+	fix?: boolean
 }>()
 </script>
 
 <template>
-	<Button :icon="icon">
-		<NuxtLink :to="to">
+	<NuxtLink :to="to">
+		<Button :icon="icon" :fix="fix">
 			<slot />
-		</NuxtLink>
-	</Button>
+		</Button>
+	</NuxtLink>
 </template>
 
 <style scoped lang="scss">
+@use "~/assets/mobile";
 a {
 	color: var(--colour-background-1);
 	text-decoration: none;
+}
+button {
+	font-size: inherit;
+	@media (max-width: mobile.$width) {
+		&:not(.fix) {
+			font-size: 1.5em;
+		}
+	}
 }
 </style>
