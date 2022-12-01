3f307d65e2fd98605c56c482d7d3ba61b43e72b4
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2022-12-01 01:30:35 +0000

    Search page simple post preview

---
+++ components/knowledge/post.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+defineProps<{
+	uid: number
+	title: string
+	preview: string
+
+	owner: User
+	topics: Topic[]
+	created: Date
+}>()
+</script>
+
+<template>
+	<article>
+		<aside>
+			<ButtonNuxt
+				:to="`/knowledge/search/?q=&topic=${topics[0].uid}`"
+				icon="material-symbols:topic-outline-rounded"
+				>{{ topics[0].name }}</ButtonNuxt
+			>
+			<UserIcon :name="owner.name" />
+			<span>{{ created.toDateString() }}</span>
+		</aside>
+		<main>
+			<h2>{{ title }}</h2>
+			<hr />
+			<div class="preview">{{ preview }}</div>
+			<ButtonNuxt
+				:to="`/knowledge/post/${uid}`"
+				icon="material-symbols:visibility-outline-rounded"
+				>View</ButtonNuxt
+			>
+		</main>
+	</article>
+</template>
+
+<style scoped lang="scss">
+article {
+	background-color: var(--colour-background-2);
+}
+</style>
--- pages/knowledge/search.vue
+++ pages/knowledge/search.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { emailDomain } from "@/types/user"
+
 definePageMeta({
 	name: "Knowledge Search Results",
 })
@@ -7,15 +9,39 @@ const route = useRoute()
 const topic = ref<Topic>()
 
 watchEffect(async () => {
+	if (!route.query.topic) {
+		topic.value = {
+			uid: -1,
+			name: "",
+		}
+		return
+	}
 	const response = await $fetch(`/api/topic/${route.query.topic}`)
 	topic.value = response
 })
+
+const posts: Post[] = [
+	{
+		uid: 12,
+		owner: { uid: 2, email: `queen${emailDomain}`, name: "Queen" },
+		topics: [
+			{
+				uid: 1,
+				name: "Topic A",
+			},
+		],
+		title: "Post Title",
+		created: new Date(Date.now()),
+		markdown: "My funky markdown",
+	},
+]
 </script>
 
 <template>
 	<KnowledgeSearchbar id="searchbar" />
 	<h3>Query: {{ $route.query.q }}</h3>
 	<h3>Topic: {{ topic?.name }}</h3>
+	<KnowledgePost v-for="post in posts" v-bind="post" :preview="post.markdown" />
 </template>
 
 <style scoped lang="scss">
