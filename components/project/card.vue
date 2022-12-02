7bb59979b29bdf9deeb31dd211a69d71d43cb4db
ccorcimaru <calincor@gmail.com>
2022-12-02 14:07:11 +0000

    initial template for card component

--- components/project/card.vue
+++ components/project/card.vue
@@ -1,40 +1,23 @@
 <script setup lang="ts">
-defineProps<{
-	title: string
-	content: string
-}>()
+defineProps<{}>()
 </script>
 
 <template>
-	<div class="card-small">
-		<h3>{{ title }}</h3>
-		<div class="flex-col-center">
-			<p>{{ content }}</p>
-		</div>
+	<div class="card flex-col flex-centre">
+		<header>
+			<slot name="title">Card title</slot>
+		</header>
+		<main class="flex-col flex-centre">
+			<slot name="largeContent"></slot>
+
+			<slot name="smallText"> small text</slot>
+		</main>
+		<footer>
+			<slot name="footerText">Footer text</slot>
+		</footer>
 	</div>
 </template>
 
 <style scoped lang="scss">
-:root {
-	--colour-highlight: #fff;
-	--card-small-radius: 14px;
-	--card-shadow: 0 4px 4px rgb(0, 0, 0, 0.25);
-}
-
-.card-small {
-	width: 100%;
-	max-width: 30ch;
-	margin: 0.5rem;
-	background-color: var(--colour-highlight);
-	padding: 1rem;
-	border-radius: var(--card-small-radius);
-	box-shadow: var(--card-shadow);
-}
-
-.flex-col-center {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	text-align: center;
-}
+@use "~/assets/core";
 </style>
