d372d00a5a83258672f3a915bf03fb9349490b8d
Arshad-59 <arshadchati2@gmail.com>
2022-12-02 23:08:58 +0000

    Created modal component
    created re-usable modal for edit profile page

---
+++ components/modal.vue
@@ -0,0 +1,31 @@
+<script setup></script>
+
+<template>
+	<div class="backdrop">
+		<div class="modal">
+			<slot name="popup-title"></slot>
+			<slot name="popup-text"></slot>
+			<slot name="popup-buttons"></slot>
+		</div>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.modal {
+	width: 28rem;
+	height: 13rem;
+	padding: 1.25rem;
+	margin: 6.25rem auto;
+	background: white;
+	border-radius: 0.625rem;
+	text-align: center;
+}
+.backdrop {
+	top: 0;
+	position: fixed;
+	background: rgba($color: black, $alpha: 0.5);
+	width: 100%;
+	height: 100%;
+	left: 0;
+}
+</style>
---
+++ pages/profile/edit/profile.vue
@@ -0,0 +1,28 @@
+<script setup>
+definePageMeta({
+	name: "Profile",
+})
+</script>
+
+<template>
+	<p>The Edit Profile Page</p>
+	<Modal class="align">
+		<template #popup-title>
+			<h2>Upload Photo</h2>
+		</template>
+		<template #popup-text>
+			<p>Upload a photo of your choice</p>
+		</template>
+		<template #popup-buttons>
+			<button>Upload</button>
+		</template>
+	</Modal>
+</template>
+
+<style scoped lang="scss">
+.align {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+</style>
