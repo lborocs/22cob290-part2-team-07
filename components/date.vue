e84b94a0ca6d9aabe82bd2e60e2c1fb1722bf215
TWoolhouse <thomasrwoolhouse@gmail.com>
2022-12-02 12:11:25 +0000

    post preview

---
+++ components/date.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const { format = "short" } = defineProps<{
+	date: DateNumber
+	format?: Intl.DateTimeFormatOptions["dateStyle"]
+}>()
+</script>
+
+<template>
+	<span>{{
+		new Date(date).toLocaleDateString(undefined, {
+			dateStyle: format,
+		})
+	}}</span>
+</template>
+
+<style scoped lang="scss"></style>
