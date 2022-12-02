28fb5552cd1c8d37d47606cfbf5933c171931bc4
ccorcimaru <calincor@gmail.com>
2022-12-02 15:58:20 +0000

    added correct classes using SCSS

--- components/project/card.vue
+++ components/project/card.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
+import { extend } from "@vue/shared"
+
 defineProps<{}>()
 </script>
 
 <template>
-	<div class="card flex-col flex-centre">
+	<div class="card flex-col centre">
 		<header>
 			<slot name="title">Card title</slot>
 		</header>
-		<main class="flex-col flex-centre">
+		<main class="flex-col centre">
 			<slot name="largeContent"></slot>
 
 			<slot name="smallText"> small text</slot>
@@ -19,5 +21,13 @@ defineProps<{}>()
 </template>
 
 <style scoped lang="scss">
-@use "~/assets/core";
+@use "/assets/core.scss" as *;
+
+.flex-col {
+	@extend %flex-col;
+}
+
+.centre {
+	@extend %flex-centre;
+}
 </style>
--- pages/project.vue
+++ pages/project.vue
@@ -8,43 +8,39 @@ const projects = [
 		title: "Project 1",
 		content: "This is the first project",
 	},
-	{
-		title: "Project 2",
-		content: "This is the second project",
-	},
-	{
-		title: "Project 3",
-		content: "This is the third project",
-	},
-	{
-		title: "Project 4",
-		content: "This is the fourth project",
-	},
-	{
-		title: "Project 5",
-		content: "This is the fifth project",
-	},
 ]
 </script>
 
 <template>
 	<p>The project page</p>
-
-	<div class="flex-row flex-centre">
-		<ProjectCard v-for="project in projects">
-			<template #title>
-				<h3>{{ project.title }}</h3>
-			</template>
-			<template #smallText>
-				<p>{{ project.content }}</p>
-			</template>
-			<template #footerText>
-				<p>footer text</p>
-			</template>
-		</ProjectCard>
-	</div>
+	<main>
+		<div class="flex-row centre">
+			<ProjectCard>
+				<template #title>
+					<h3>Project Progress</h3>
+				</template>
+				<template #largeContent>
+					<ProjectSpinner />
+				</template>
+				<template #smallText>
+					<p></p>
+				</template>
+				<template #footerText>
+					<p>footer tex</p>
+				</template>
+			</ProjectCard>
+		</div>
+	</main>
 </template>
 
 <style scoped lang="scss">
-@use "~/assets/core";
+@use "/assets/core.scss" as *;
+
+.flex-row {
+	@extend %flex-row;
+}
+
+.centre {
+	@extend %flex-centre;
+}
 </style>
