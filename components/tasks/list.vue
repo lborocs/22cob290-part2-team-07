227e3464a908a6b1c129027fc7678714492ba057
Adam Hutton <adam1.hutton1@gmail.com>
2022-12-04 17:43:18 +0000

    task row

--- components/tasks/list.vue
+++ components/tasks/list.vue
@@ -2,8 +2,20 @@
 	<div class="task-list">
 		<div class="task-row" v-for="task in tasks" :key="task.uid">
 			<h3>{{ task.name }}</h3>
+			<p class="details">{{ task.description }}</p>
 
-			<AvatarStack :array="task.assignees.slice(0, 3)" />
+			<div class="end-details">
+				<AvatarStack :array="task.assignees.slice(0, 3)" />
+
+				<p class="details">
+					<Icon icon="material-symbols:hourglass-bottom-rounded" />
+					{{ task.workerHours }} Hours
+				</p>
+				<p class="details">
+					<Icon icon="material-symbols:calendar-month-outline-rounded" />
+					{{ new Date(task.deadline).toLocaleDateString() }}
+				</p>
+			</div>
 		</div>
 	</div>
 </template>
@@ -20,13 +32,30 @@
 	align-items: center;
 	gap: 1rem;
 
+	h3 {
+		padding-right: 0.5rem;
+	}
+
 	&:not(:last-child) {
 		border-bottom: 1px solid var(--colour-text);
 	}
 }
+
+.end-details {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 1rem;
+	margin-left: auto;
+}
+.details {
+	color: var(--colour-text-faded);
+}
 </style>
 
 <script setup lang="ts">
+import { Icon } from "@iconify/vue"
+
 defineProps<{
 	tasks: Task[]
 }>()
