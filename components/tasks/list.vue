61d511f15b32cad5692fe091df1c9b8d54ee939d
Adam Hutton <adam1.hutton1@gmail.com>
2022-12-04 18:29:07 +0000

    task list completion style

--- components/tasks/list.vue
+++ components/tasks/list.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="task-list">
 		<div class="task-row" v-for="task in tasks" :key="task.uid">
+			<input type="checkbox" name="completed" class="checkbox" />
 			<div class="project details">
 				<span v-if="task.project" class="project-title">
 					<Icon icon="material-symbols:push-pin-outline" />
@@ -35,10 +36,12 @@
 
 .task-row {
 	display: grid;
-	grid-template-columns: 0.8fr 1fr 3fr 1fr 1fr 0.5fr;
+	--cb-size: 1.5rem;
+	grid-template-columns: var(--cb-size) 0.8fr 1fr 3fr 1fr 1fr 0.5fr;
 	flex-direction: row;
 	align-items: center;
 	gap: 1rem;
+	position: relative;
 
 	h3 {
 		padding-right: 0.5rem;
@@ -46,6 +49,11 @@
 
 	&:not(:last-child) {
 		border-bottom: 1px solid var(--colour-text);
+	}
+
+	&:has(.checkbox:checked) {
+		opacity: 0.5;
+		text-decoration: line-through;
 	}
 }
 
@@ -75,6 +83,12 @@
 	-webkit-line-clamp: 2; /* number of lines to show */
 	line-clamp: 2;
 	-webkit-box-orient: vertical;
+}
+
+.checkbox {
+	width: var(--cb-size);
+	height: var(--cb-size);
+	accent-color: var(--colour-accent);
 }
 </style>
 
