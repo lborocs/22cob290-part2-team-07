72dde7204ddec7e1b623e70bb927bca7c0f97900
ccorcimaru <calincor@gmail.com>
2022-12-05 14:15:12 +0000

    added "profile-picture" class to user icon

---
+++ components/project/member.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+defineProps<{
+	uid: number
+	name: string
+	rank: string
+}>()
+</script>
+
+<template>
+	<div class="flex-col card-small">
+		<a> </a>
+	</div>
+</template>
+
+<style lang="scss">
+@use "/assets/core.scss" as *;
+
+.flex-col {
+	@extend %flex-col;
+}
+
+.card-small {
+	@extend .card-small;
+}
+</style>
