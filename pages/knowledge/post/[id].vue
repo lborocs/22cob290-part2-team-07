6c72809dc344d5731ed0856fa7069fcbb9e3065f
TWoolhouse <thomasrwoolhouse@gmail.com>
2022-12-08 18:07:45 +0000

    can switch a post to edit mode

--- pages/knowledge/post/[id].vue
+++ pages/knowledge/post/[id].vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import { rankTitle } from "@/types/user"
 
+definePageMeta({
+	name: "Post Name",
+})
+
 const route = useRoute()
 const { data: post } = await useFetch(`/api/post/${route.params.id}`)
 
-// const
+// If the current user has access to edit the post
+const editor = true
+const isEditing = $computed(() => route.path.endsWith("edit"))
 </script>
 
 <template>
@@ -20,8 +26,38 @@ const { data: post } = await useFetch(`/api/post/${route.params.id}`)
 		<template #header>
 			<h2>{{ post.title }}</h2>
 		</template>
+		<template #header-control>
+			<div class="control">
+				<ButtonNuxt
+					v-if="editor && !isEditing"
+					icon="material-symbols:topic-outline-rounded"
+					:to="`/knowledge/search/?topic=${post.topic.uid}`"
+					>{{ post.topic.name }}</ButtonNuxt
+				>
+			</div>
+		</template>
 		<slot />
 		<template #footer><Date class="date" :date="post.created" /></template>
+		<template #footer-control>
+			<div class="control" v-if="editor && !isEditing">
+				<ButtonNuxt
+					icon="material-symbols:edit-outline-rounded"
+					:to="`/knowledge/post/${post.uid}/edit`"
+					>Edit</ButtonNuxt
+				>
+			</div>
+			<div class="control" v-if="editor && isEditing">
+				<ButtonNuxt
+					icon="material-symbols:cancel-outline-rounded"
+					:to="`/knowledge/post/${post.uid}`"
+					>Cancel</ButtonNuxt
+				><ButtonNuxt
+					icon="material-symbols:save-outline-rounded"
+					:to="`/knowledge/post/${post.uid}`"
+					>Save</ButtonNuxt
+				>
+			</div>
+		</template>
 		<NuxtPage :post="post" />
 	</Sandwich>
 	<KnowledgePostMissing v-else />
@@ -62,5 +98,9 @@ h2,
 .date {
 	color: var(--colour-accent);
 	margin: 0;
+}
+
+.control {
+	display: flex;
 }
 </style>
