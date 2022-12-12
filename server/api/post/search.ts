c198957eca356e209fdf396e6a6e4024cce3a935
TWoolhouse <thomasrwoolhouse@gmail.com>
2022-12-12 19:43:32 +0000

    api: post searching

---
+++ server/api/post/search.ts
@@ -0,0 +1,29 @@
+import prisma from "@/prisma"
+
+export default defineCachedEventHandler(event => {
+	const query = getQuery(event)
+	console.log(query)
+
+	const topic = {
+		uid: +(query.topic as string),
+	}
+
+	const title = {
+		contains: query.q as string,
+	}
+
+	const where: {
+		title?: typeof title
+		topic?: typeof topic
+	} = {}
+	if (query.q) where.title = title
+	if (query.topic) where.topic = topic
+
+	return prisma.post.findMany({
+		where,
+		include: {
+			owner: true,
+			topic: true,
+		},
+	})
+})
