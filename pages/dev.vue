6b8591bbd391dbbee42fa8b02e95c0e258384e02
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2022-12-13 23:11:40 +0000

    added dev page to quickly refresh the db

---
+++ pages/dev.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+let activeProcesses = $ref(0)
+const disable = $computed(() => activeProcesses > 0)
+
+async function refreshDatabase() {
+	activeProcesses++
+	await useLazyFetch("/api/faux?refresh")
+	activeProcesses--
+}
+</script>
+
+<template>
+	<Button
+		icon="material-symbols:refresh-rounded"
+		:fix="true"
+		@click="refreshDatabase"
+		:disabled="disable"
+		>Refresh Database</Button
+	>
+</template>
+
+<style scoped lang="scss"></style>
