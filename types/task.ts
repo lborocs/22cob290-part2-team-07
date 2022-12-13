2737686ad6fb19bf7f09622f1f406232c12a480e
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2022-12-13 23:43:12 +0000

    worker hours calculation

--- types/task.ts
+++ types/task.ts
@@ -1,6 +1,14 @@
+import { Subtask, Task } from ".prisma/client"
 import { TaskStatus } from "."
 
 const statuses = ["Todo", "In Progress", "Done"]
 export function statusName(status: TaskStatus): string {
 	return statuses[status]
+}
+
+export function workerHours(task: Task & { subtasks: Subtask[] }) {
+	return task.subtasks.reduce(
+		(total, subtask) => total + subtask.workerHours,
+		0,
+	)
 }
