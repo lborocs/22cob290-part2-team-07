040ce7be50798a5abadb4200ae00839c071b982a
TheRozza <r.murphy1601@gmail.com>
2022-12-13 20:46:02 +0000

    Created Login form

---
+++ components/LoginReg/LoginForm.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+	<!--might not be the way i do this, just seeing what works-->
+	<Form to="/dashboard">
+		<label for="email">Email:</label>
+		<input type="email" id="email" placeholder="Example@make-it-all.com" />
+		<label for="Password">Password:</label>
+		<input type="password" id="Password" />
+		<Button type="submit" icon="">Login</Button>
+	</Form>
+</template>
+
+<style scoped lang="scss"></style>
