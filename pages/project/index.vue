94d1dc1279ee4292c6c226a9ca87c94aa2234eac
TWoolhouse <thomasrwoolhouse@gmail.com>
2022-12-14 13:05:07 +0000

    projects page as an index to all projects the user has access to see

---
+++ pages/project/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { data: projects } = await useFetch("/api/projects")
+</script>
+
+<template>
+	<ButtonNuxt
+		v-for="project in projects"
+		:key="project.uid"
+		:to="`/project/${project.uid}`"
+		icon="material-symbols:folder-outline-rounded"
+		>{{ project.name }}</ButtonNuxt
+	>
+</template>
+
+<style scoped lang="scss"></style>
