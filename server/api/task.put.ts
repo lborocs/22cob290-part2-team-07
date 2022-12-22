ed38246d99fd3c51f6db94557d9b83d7f19903b2
Adam Hutton <adam1.hutton1@gmail.com>
2022-12-22 12:51:13 +0000

    task creation api reads req body

--- server/api/task.put.ts
+++ server/api/task.put.ts
@@ -1,20 +1,32 @@
 import prisma from "@/prisma"
+import { Task } from "@prisma/client"
+import { PrismaClientValidationError } from "@prisma/client/runtime"
 
 export default defineEventHandler(async event => {
 	const query = getQuery(event)
 	console.log("creating task")
+	try {
+		const body = await readBody(event)
 
-	const task = await prisma.task.create({
-		data: {
-			description: "hard-coded description",
-			name: "Task Name (hc)",
-			workerHours: 12,
-			assignees: {},
-			subtasks: {},
-			project: {},
-		},
-	})
-	console.log(task)
+		const details = body.task as Task
+		console.table(details)
 
-	return { success: true }
+		const task = await prisma.task.create({
+			data: {
+				name: details.name,
+				description: details.description,
+				workerHours: details.workerHours,
+			},
+		})
+		console.log("Task created - ", task)
+
+		return { success: true }
+	} catch (e) {
+		if (e instanceof PrismaClientValidationError) {
+			console.error(
+				"PrismaClientValidationError - probably missing fields for task",
+			)
+			return { success: false, error: "Missing fields for task" }
+		}
+	}
 })
