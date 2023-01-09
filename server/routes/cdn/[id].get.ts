811bd0699f21fc726e4a55226144613144157067
TWoolhouse <thomasrwoolhouse@gmail.com>
2023-01-09 10:55:14 +0000

    cdn will set the HTTP status code when the uid does not exist

--- server/routes/cdn/[id].get.ts
+++ server/routes/cdn/[id].get.ts
@@ -12,19 +12,23 @@ export default defineEventHandler(async event => {
 	const asset = await prisma.asset.findUnique({
 		where: { uid: event.context.params.id as string },
 	})
-	event.node.res.setHeader("Content-Type", asset!.mimeType)
-	event.node.res.setHeader(
-		"Content-Disposition",
-		`inline; filename="${asset!.name}"`,
-	)
-	event.node.res.setHeader("Cache-Control", CACHE_HEADER)
 	event.node.res.setHeader("Connection", "keep-alive")
 	event.node.res.setHeader("Keep-Alive", "timeout=5, max=30")
-	const filepath = path.join(CDN, asset!.uid)
-	return new Promise<void>(resolve => {
-		readFile(filepath, { encoding: "binary" }, (err, data) => {
-			event.node.res.end(data, "binary")
-			resolve()
+	if (asset) {
+		event.node.res.setHeader("Content-Type", asset.mimeType)
+		event.node.res.setHeader(
+			"Content-Disposition",
+			`inline; filename="${asset.name}"`,
+		)
+		event.node.res.setHeader("Cache-Control", CACHE_HEADER)
+		const filepath = path.join(CDN, asset.uid)
+		return new Promise<void>(resolve => {
+			readFile(filepath, { encoding: "binary" }, (err, data) => {
+				event.node.res.end(data, "binary")
+				resolve()
+			})
 		})
-	})
+	} else {
+		event.node.res.statusCode = 404
+	}
 })
