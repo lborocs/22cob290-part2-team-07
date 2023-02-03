6e1a60566ff30a50008c0cbf4a7d6d217cc9baf4
Adam Hutton <adam1.hutton1@gmail.com>
2023-02-03 12:41:40 +0000

    subtasks change parent status (after refresh)

--- server/api/subtask/[id].put.ts
+++ server/api/subtask/[id].put.ts
@@ -4,10 +4,28 @@ import prisma from "~~/prisma"
 export default defineEventHandler(async event => {
 	const body = await readBody(event)
 	const newStatus = body as boolean
-	await prisma.subtask.update({
+	const subtask = await prisma.subtask.update({
 		where: { uid: +(event.context.params.id as number) },
 		data: { done: newStatus },
 	})
+	const allSubtasks = await prisma.subtask.findMany({
+		where: { parentId: subtask.parentId },
+	})
+	const allDone = allSubtasks.filter(subtask => subtask.done)
+
+	let newParentStatus: number
+	if (allDone.length === allSubtasks.length) {
+		newParentStatus = 2
+	} else if (allDone.length === 0) {
+		newParentStatus = 0
+	} else {
+		newParentStatus = 1
+	}
+	const res = await prisma.task.update({
+		where: { uid: subtask.parentId },
+		data: { status: newParentStatus },
+	})
+
 	return {
 		status: 200,
 	}
