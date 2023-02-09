0cc1efd81d30278d59ac5cdc945bac9a5aef6839
TWoolhouse <thomasrwoolhouse@gmail.com>
2023-02-09 23:53:45 +0000

    user roles

--- server/api/user/[email].ts
+++ server/api/user/[email].ts
@@ -3,5 +3,13 @@ import prisma from "@/prisma"
 export default defineEventHandler(event => {
 	return prisma.user.findUnique({
 		where: { email: event.context.params.email as string },
+		include: {
+			roles: {
+				select: {
+					name: true,
+					rank: true,
+				},
+			},
+		},
 	})
 })
