7619f7d782fbfa23f68a69fa63d55e3163bfebd7
ccorcimaru <calincor@gmail.com>
2023-02-09 23:54:00 +0000

    temp fix for broken roles

--- components/project/member.vue
+++ components/project/member.vue
@@ -14,7 +14,7 @@ defineProps<{
 			<UserIcon :email="user.email" :name="user.name" :size="50" />
 			<h3 class="card-margins">{{ user.name }}</h3>
 		</NuxtLink>
-		<p class="dimmed card-margins">{{ rankTitle(user.rank) }}</p>
+		<p class="dimmed card-margins">Employee</p>
 		<p class="card-margins">
 			Assigned hours
 			<span class="detail-highlight"> {{ assigned ? assigned : "0" }} </span>
