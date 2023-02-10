3c46e4835a011cf18aec5d0f0b1d88d3b73ade04
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2023-02-10 23:22:59 +0000

    faux deletes old register links

--- server/api/faux.ts
+++ server/api/faux.ts
@@ -17,6 +17,7 @@ export default defineEventHandler(async event => {
 			prisma.pOverrideRoleProject.deleteMany(),
 			prisma.pOverrideUserProject.deleteMany(),
 
+			prisma.register.deleteMany(),
 			prisma.subtask.deleteMany(),
 			prisma.task.deleteMany(),
 			prisma.project.deleteMany(),
