d1f70369460555fc4b7bfb922f346fd932ac9f6c
ccorcimaru <calincor@gmail.com>
2023-02-14 22:54:14 +0000

    initial hover tooltip

--- components/user/icon.vue
+++ components/user/icon.vue
@@ -12,23 +12,36 @@ withDefaults(
 		isLink: true,
 	},
 )
+
+const showDiv = ref(false)
 </script>
 
 <template>
-	<NuxtLink v-if="isLink" :to="`/user/${email}`"
-		><img
+	<div
+		class="profile-picture-container"
+		@mouseover="showDiv = true"
+		@mouseleave="showDiv = false"
+	>
+		<NuxtLink v-if="isLink" :to="`/user/${email}`">
+			<img
+				:src="profilePicture(name)"
+				:alt="`Profile Picture of ${name}`"
+				:width="size"
+				:height="size"
+			/>
+		</NuxtLink>
+		<img
+			v-else
 			:src="profilePicture(name)"
 			:alt="`Profile Picture of ${name}`"
 			:width="size"
 			:height="size"
-	/></NuxtLink>
-	<img
-		v-else
-		:src="profilePicture(name)"
-		:alt="`Profile Picture of ${name}`"
-		:width="size"
-		:height="size"
-	/>
+		/>
+		<div v-if="showDiv" class="profile-info">
+			<p>{{ name }}</p>
+			<p>{{ email }}</p>
+		</div>
+	</div>
 </template>
 
 <style scoped lang="scss">
@@ -36,5 +49,31 @@ withDefaults(
 
 img {
 	@extend %profile-picture;
+}
+
+.profile-picture-container {
+	position: relative;
+	display: inline-block;
+
+	.profile-info {
+		position: absolute;
+		top: -20%;
+		left: 20%;
+		transform: translateX(10%) translateY(-30%);
+		background-color: #fff;
+		border: 1px solid #ddd;
+		padding: 8px;
+		border-radius: 4px;
+		box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.3);
+		white-space: nowrap;
+		z-index: 1;
+	}
+
+	.profile-name {
+		font-weight: bold;
+	}
+	.profile-email {
+		color: #888;
+	}
 }
 </style>
