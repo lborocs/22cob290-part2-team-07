0e092c5112b4e3a056a2c2e077a9b05d21426160
TheRozza <r.murphy1601@gmail.com>
2023-02-16 15:41:52 +0000

    Fixed some of the hydration errors
    There is still one that I cannot work out why its breaking. There is also some on the manager dashboard, as the issue is with the employee overview card

--- components/project/member.vue
+++ components/project/member.vue
@@ -9,17 +9,19 @@ defineProps<{
 </script>
 
 <template>
-	<div class="flex-col card-small">
-		<NuxtLink :to="`/user/${user.uid}`">
-			<UserIcon :email="user.email" :name="user.name" :size="50" />
-			<h3 class="card-margins">{{ user.name }}</h3>
-		</NuxtLink>
-		<p class="dimmed card-margins">{{ rolesTitle(user.roles) }}</p>
-		<p class="card-margins">
-			Assigned hours
-			<span class="detail-highlight"> {{ assigned ? assigned : "0" }} </span>
-		</p>
-	</div>
+	<ClientOnly>
+		<div class="flex-col card-small">
+			<NuxtLink :to="`/user/${user.uid}`">
+				<UserIcon :email="user.email" :name="user.name" :size="50" />
+				<h3 class="card-margins">{{ user.name }}</h3>
+			</NuxtLink>
+			<p class="dimmed card-margins">{{ rolesTitle(user.roles) }}</p>
+			<p class="card-margins">
+				Assigned hours
+				<span class="detail-highlight"> {{ assigned ? assigned : "0" }} </span>
+			</p>
+		</div>
+	</ClientOnly>
 </template>
 
 <style scoped lang="scss">
