96315d49204dd201eabd79a61d5b7fc25f34b77e
Adam Hutton <adam1.hutton1@gmail.com>
2023-02-16 10:34:51 +0000

    fix clients api route to return all clients

--- server/api/clients.ts
+++ server/api/clients.ts
@@ -1,12 +1,5 @@
 import prisma from "@/prisma"
 
 export default defineEventHandler(async event => {
-	return await prisma.client.findUnique({
-		where: {
-			uid: +(event.context.params.id as string),
-		},
-		include: {
-			projects: true,
-		},
-	})
+	return await prisma.client.findMany()
 })
