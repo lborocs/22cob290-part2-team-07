97e67c2ba2a663cd1e13ace5d728a459c509d2a9
TWoolhouse <thomasrwoolhouse@gmail.com>
2023-02-17 12:13:58 +0000

    topic permissions pages

---
+++ pages/knowledge/topic/[uid]/permission/role/[role].vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { PermissionCollection } from "@/types/permission"
+definePageMeta({
+	name: "Topic Role Permissions",
+})
+</script>
+
+<template>
+	<PermissionOverrideRole
+		table="topic"
+		:table-uid="+$route.params.uid"
+		:role-uid="+$route.params.role"
+		:permissions="PermissionCollection.Topic | PermissionCollection.Post"
+	/>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ pages/knowledge/topic/[uid]/permission/user/[email].vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { PermissionCollection } from "@/types/permission"
+
+definePageMeta({
+	name: "Topic User Permissions",
+})
+
+const route = useRoute()
+const { data: user } = await useFetch(`/api/user/${route.params.email}`)
+</script>
+
+<template>
+	<PermissionOverrideUser
+		table="topic"
+		:table-uid="+$route.params.uid"
+		:user="user"
+		:permissions="PermissionCollection.Topic | PermissionCollection.Post"
+		v-if="user"
+	/>
+</template>
+
+<style scoped lang="scss"></style>
