64e9563a2bd6c706f31f8db77bfeba2fb9dcd6d1
TWoolhouse <thomasrwoolhouse@gmail.com>
2023-02-17 10:40:54 +0000

    simpler api route

--- server/api/user/uid/[uid].get.ts
+++ server/api/user/uid/[uid].get.ts
@@ -1,14 +1,13 @@
 import prisma from "@/prisma"
 
 export default defineCachedEventHandler(
-	event => {
-		return prisma.user.findUnique({
+	event =>
+		prisma.user.findUnique({
 			where: { uid: event.context.params.uid as string },
 			include: {
 				roles: true,
 			},
-		})
-	},
+		}),
 	{
 		maxAge: 5,
 	},
